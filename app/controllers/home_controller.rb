d1d7231e90e57bce88cb5712c1baa32aeb78dc76
Sean Rankin <sean.rankin@gmail.com>
2016-08-19 12:03:43 -0400

    Added test page for skeleton + image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,6 @@ class HomeController < ApplicationController
   end
   def math
   end
+  def test
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "/credits", to: "home#credits"
   get "/math", to: "home#math"
+  get "/test", to: "home#test"
 end
